71bf30d0a43e8349564daa9c44d429f2191fb623
k020c1357 <k020c1357@users.noreply.github.com>
2022-01-31 02:15:37 +0000

    deploy: 0309e237ed0775adbb33e3760dba76da24f57179

--- src/core/store/actions/search.ts
+++
@@ -1,8 +0,0 @@
-import { UPDATE_SEARCH } from '../../types/actions';
-
-export const updateSearch = (search: string) => {
-  return {
-    type: UPDATE_SEARCH,
-    payload: { search },
-  };
-};
--- src/core/store/actions/userPassword.ts
+++
@@ -1,8 +0,0 @@
-import { CREATE_USER_PASSWORD_ACCESS } from '../../types/actions';
-
-export const createOrEditPassword = (password: String) => {
-  return {
-    type: CREATE_USER_PASSWORD_ACCESS,
-    payload: { password },
-  };
-};
